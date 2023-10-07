df2f543e7afe16abab596e5f2c4b650dfea1514e
Борисовская Алена <aalborisovskaya@sberbank.ru>
2023-10-07 18:03:56 +0300

    feat: Add dependencies, suspend, coro scope

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,5 +1,9 @@
 package otus.homework.coroutines
 
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,18 +15,22 @@ class CatsPresenter(
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        CoroutineScope(Dispatchers.IO).launch {
+            val fact = catsService.getCatFact()
+            Log.d("TAG", "Fact is $fact")
+        }
+//        .enqueue(object : Callback<Fact> {
+//
+//            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
+//                if (response.isSuccessful && response.body() != null) {
+//                    _catsView?.populate(response.body()!!)
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<Fact>, t: Throwable) {
+//                CrashMonitor.trackWarning()
+//            }
+//        })
     }
 
     fun attachView(catsView: ICatsView) {
