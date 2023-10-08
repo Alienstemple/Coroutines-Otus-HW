5b687b86cab9c47c6af256c2df3762c6f166a81c
Борисовская Алена <aalborisovskaya@sberbank.ru>
2023-10-08 12:12:46 +0300

    feat: Add coro-android, populate view on Main Dispatcher

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,9 +1,11 @@
 package otus.homework.coroutines
 
 import android.util.Log
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,9 +17,14 @@ class CatsPresenter(
     private var _catsView: ICatsView? = null
 
     fun onInitComplete() {
-        CoroutineScope(Dispatchers.IO).launch {
+        val presenterScope = CoroutineScope(Dispatchers.IO + CoroutineName("CatsCoroutine"))
+        presenterScope.launch {
             val fact = catsService.getCatFact()
-            Log.d("TAG", "Fact is $fact")
+            val newFact = Fact(createdAt = "", deleted = false, id = "myCustomId", text = "myCustom", source = "", used = false, type = "", user = "", updatedAt = "")
+            withContext(Dispatchers.Main) {
+                _catsView?.populate(newFact)
+            }
+
         }
 //        .enqueue(object : Callback<Fact> {
 //
